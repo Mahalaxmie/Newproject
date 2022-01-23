12bdff7f95b44f6b8c85d8b7ab2727346da1458f
mahalaxmie <cmaha.dpm@gmail.com>
2022-01-23 11:06:57 +0530

    task done

---
+++ src/org/git/GitProject.java
@@ -0,0 +1,14 @@
+package org.git;
+
+public class GitProject {
+	
+	private void add() {
+		System.out.println("add");
+
+	}
+	public static void main(String[] args) {
+		GitProject g = new GitProject();
+		g.add();
+	}
+
+}
